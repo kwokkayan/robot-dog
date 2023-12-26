ad73e9205edf70acd7b782ce1796a4d2221eac8b
Terry Leung <59078445+terryleungcc@users.noreply.github.com>
2023-12-26 17:25:47 +0800

    feat: add ydlidar_ros_driver package

---
+++ lidar/ydlidar_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/terryl/git/robot-dog/lidar/ydlidar_ws/src
+# Build directory: /home/terryl/git/robot-dog/lidar/ydlidar_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ydlidar_ros_driver")
