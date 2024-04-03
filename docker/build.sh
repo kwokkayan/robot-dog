9eca4dfe5bb0155c4132f1a64b95fc3f964e8894
foo bar <foo.bar@users.noreply.github.com>
2024-04-03 23:33:47 +0800

    feat: add docker folder for deployment

---
+++ docker/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+TARGET="$1"
+
+PLATFORM="$(uname -m)"
+
+BASE_NAME="ros_custom-$TARGET-$PLATFORM"
+
+# Build the image
+docker buildx build ./docker \
+    --file ./docker/Dockerfile.$TARGET \
+    --build-arg USERNAME=admin \
+    --build-arg USER_UID=1000 \
+    --build-arg USER_GID=1000 \
+    --build-arg SSH_PRV_KEY="$(cat ~/.ssh/id_rsa)" \
+    --build-arg SSH_PUB_KEY="$(cat ~/.ssh/id_rsa.pub)" \
+    --platform $PLATFORM \
+    --tag $BASE_NAME 
